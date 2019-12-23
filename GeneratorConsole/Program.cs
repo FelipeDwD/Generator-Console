83f1bf4011198578b2afae2a638af5e9f0db9adc
Felipe Neves de Arruda <50635026+FelipeDwD@users.noreply.github.com>
2019-12-23 11:26:12 -0300

    Adicionar arquivos de projeto.

---
+++ GeneratorConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace GeneratorConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(@"C:\Users\FelipeN\Desktop\direct\source");
+            string directoryInfoCp = @"C:\Users\FelipeN\Desktop\direct\source\";
+
+
+            Console.WriteLine("Quantas cópias deseja fazer: ");
+            int amountFilesToGenerated = int.Parse(Console.ReadLine());
+
+            foreach (FileInfo file in directoryInfo.GetFiles())
+            {
+                for (int i = 0; i < amountFilesToGenerated; i++)
+                {
+                    File.Copy(file.FullName, $"{directoryInfoCp}CP{i}{file.Name}");
+                }
+            }
+
+            Console.WriteLine($"{amountFilesToGenerated} cópias feitas em {directoryInfoCp}");
+        }
+    }
+}
